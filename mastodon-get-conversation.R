2634f5dba3906ab8ba31941c80a05417ebb41fca
ziemek-bucko <60089915+ziemek-bucko@users.noreply.github.com>
2022-12-20 16:18:19 +0100

    added explanation in comments

--- mastodon-get-conversation.R
+++ mastodon-get-conversation.R
@@ -5,7 +5,7 @@ require(jsonlite)
 require(dplyr)
 require(purrr)
 
-server = "your-server"
+server = "your-server" #The Mastodon server you want to access, e.g., "mastodon.social"
 status_id <- ('your-status') #The ID of the status you want to get replies for - it's the last part of the URL
 url_handle <-
   paste0("https://", server, "/api/v1/statuses/", status_id, ":id/context/")
