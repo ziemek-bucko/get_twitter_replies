12672a5815a27f49bb42c0ffc0b8cb41ccfb54cf
ziemek-bucko <60089915+ziemek-bucko@users.noreply.github.com>
2022-12-20 16:16:43 +0100

    Get all statuses within a conversation on Mastodon

---
+++ mastodon-get-conversation.R
@@ -0,0 +1,19 @@
+
+require(rjson)
+require(httr)
+require(jsonlite)
+require(dplyr)
+require(purrr)
+
+server = "your-server"
+status_id <- ('your-status') #The ID of the status you want to get replies for - it's the last part of the URL
+url_handle <-
+  paste0("https://", server, "/api/v1/statuses/", status_id, ":id/context/")
+
+response <-
+  httr::GET(url = url_handle)
+obj <- httr::content(response, as = "text")
+x <- rjson::fromJSON(obj)
+
+dat = as.data.frame(unlist(lapply(unlist(x, recursive = FALSE), `[`, "content")))
+
